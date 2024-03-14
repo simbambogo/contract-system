d9b0e97788bce008ec00146f365dc86934641a07
simbambogo <yasinimusa77@gmail.com>
2024-03-14 12:18:07 +0300

    login page

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ContractController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/login', [App\Http\Controllers\ContractController::class, 'login'])->name('contract.login');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
